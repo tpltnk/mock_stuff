9531d8f51adef18cb5d20f00491d418478b40b44
tpltnk <tit.poplatnik353535@gmail.com>
2021-07-10 16:24:13 +0200

    crap123

--- main.py
+++ main.py
@@ -1,11 +1,53 @@
 import json
 import torch
-import time
 
 DATA_FILE_NAME = "MOCK_DATA.json"
+GENDER_INFO = {
+    "Male": 0,
+    "Female": 1
+}
+
+def read_mock_data() -> torch.Tensor:
+    with open(DATA_FILE_NAME) as json_file:
+        mock_data = [[GENDER_INFO[entry["gender"]], entry["likes_sandwich"]] for entry in json.load(json_file)]
+    return torch.tensor(mock_data)
+
+def get_correlations(tensor: torch.Tensor) -> dict:
+    cor = {}
+    for entry in tensor:
+        gender = int(entry[0])
+        likes = int(entry[1])
+        if not cor.get(gender):
+            cor[gender] = 0
+        cor[gender] += likes
+    v1 = (cor[0] * 100) / float((cor[0] + cor[1]))
+    v2 = (cor[1] * 100) / float((cor[0] + cor[1]))
+    cor[0] = round(v1, 3)
+    cor[1] = round(v2, 3)
+    assert cor[0] + cor[1] == 100.0
+    return cor
+
+
+class CorrelatorModel(torch.nn.Module):
+    def __init__(self, insize, outsize):
+        super().__init__()
+        self.linear = torch.nn.Linear(insize, outsize)
+
+    def forward(self, tensor):
+        out = self.linear(tensor)
+        return out
+
+class CorrelatorConvolutionModel(torch.nn.Module):
+    def __init__(self, insize, outsize):
+        super().__init__()
+        self.conv1 = torch.nn.Conv2d(insize, outsize)
+        self.conv2 = torch.nn.Conv2d(insize, outsize)
+    
+    def forward(self, tensor):
+        relu = torch.nn.ReLU
 
 if __name__ == "__main__":
-    T = torch.randint(0, 10, [10, 10])
-    T, _ = T.sort(dim=-1)
-    print(T.diag(0))
+    T = read_mock_data()
+    print(T.dim())
     print(T)
+    print(get_correlations(T))
